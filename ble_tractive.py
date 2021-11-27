fd642bc5e53a869b3a3a993fed46c449ffcb3a85
Dr. Usman Kayani <usman.kayaniphd@gmail.com>
2021-11-27 01:35:25 +0000

    ..

--- ble_tractive.py
+++ ble_tractive.py
@@ -1,25 +1,33 @@
+from typing import Optional
 import pexpect, sys, argparse
 from colorama import Fore, Back, Style 
 
 __author__ = ['Dr. Usman Kayani']
 
 
-def write_req(handle, code):
-	return f'char-write-req {handle} {code}'
+def io_req(
+	handle: str, 
+	code: Optional[str] = None,
+) -> str:
+	if code:
+		return f'char-write-req {handle} {code}'
+	else:
+		return f'char-read-uuid {handle}'
+		
 
 if __name__ == '__main__':
 
 	# parser = argparse.ArgumentParser(add_help=False)
 
 	# Device mac address.
-	DEVICE = sys.argv[1]
+	device_mac = sys.argv[1]
 
 	# Run gatttool interactively.
-	gatt = pexpect.spawn('gatttool -b ' + DEVICE + ' -t random -I')
+	gatt = pexpect.spawn(f'gatttool -b {device_mac} -t random -I')
 	
 	# Connect to the device.
 	print(Style.RESET_ALL + 'Connecting to'),
-	print(DEVICE)
+	print(device_mac)
 	flag = True
 	while flag:
 		gatt.sendline('connect')
@@ -30,14 +38,14 @@ if __name__ == '__main__':
 		except:
 			pass
 
-	command = 'char-read-uuid 00002a19-0000-1000-8000-00805f9b34fb'
+
+	command = io_req('00002a19-0000-1000-8000-00805f9b34fb')
 	gatt.sendline(command)
 	gatt.expect('handle: ', timeout=10)
 	gatt.expect('\r\n', timeout=10)
-	BATTERY = int(gatt.before[16:18], 16)
-	print('Battery level: ' + str(BATTERY) + '%')
+	print(f'Battery level: {int(gatt.before[16:18], 16)}%')
 
-	command = 'char-read-uuid c1670003-2c5d-42fd-be9b-1f2dd6681818'
+	command = io_req('c1670003-2c5d-42fd-be9b-1f2dd6681818')
 	gatt.sendline(command)
 	gatt.expect('handle: ', timeout=10)
 	gatt.expect('\r\n', timeout=10)
@@ -55,14 +63,14 @@ if __name__ == '__main__':
 				flag = False
 			if cdl.split()[0] == 'light':
 				if cdl.split()[1] == 'on':
-					command = write_req(handle, '0b00080280000000000000004b')
+					command = io_req(handle, '0b00080280000000000000004b')
 				elif cdl.split()[1] == 'off':
-					command = write_req(handle, '0b000802010000000000000001')
+					command = io_req(handle, '0b000802010000000000000001')
 			elif cdl.split()[0] == 'sound':
 				if cdl.split()[1] == 'on':
-					command = write_req(handle, '0b0019022001040102010101e0')
+					command = io_req(handle, '0b0019022001040102010101e0')
 				elif cdl.split()[1] == 'off':
-					command = write_req(handle, '0b001902010000000000000001')
+					command = io_req(handle, '0b001902010000000000000001')
 			elif cdl.split()[0] == 'exit':
 				sys.exit()
 			gatt.sendline(command)
