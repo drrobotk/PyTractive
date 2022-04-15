70515335073a43f19ff70bc91d2dcf3be1e4d682
Dr. Usman Kayani <usmankayani@Usmans-MacBook-Pro.local>
2022-04-15 11:18:22 +0100

    Added setup.py file and deleted requirements file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PyTractive',
+    version='0.1',
+    description='Python module and script to interact with the Tractive GPS tracker.',
+    author='Dr. Usman Kayani',
+    url='https://github.com/drrobotk/PyTractive',
+    author_email='usman.kayaniphd@gmail.com',
+    license='MIT',
+    packages=['PyTractive'],
+    install_requires=['geopy>=2.0', 'folium>=0.1', 'pandas>=1.0', 'Pillow>=8.0'],
+    include_package_data=True,
+)
