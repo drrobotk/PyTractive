2bcc1da30239090adb1d27a0adf774c4356b3bfa
Dr. Usman Kayani <51001263+drrobotk@users.noreply.github.com>
2023-08-03 02:36:13 +0100

    Create bluetooth_windows.py

---
+++ PyTractive/bluetooth_windows.py
@@ -0,0 +1,63 @@
+import asyncio, argparse
+from bleak import BleakClient
+
+address = "Your_Device_Address_Here"
+battery_char = '00002a19-0000-1000-8000-00805f9b34fb'
+sensor_char = "c1670003-2c5d-42fd-be9b-1f2dd6681818"
+sensor_cmds = {
+    'sound': {'on': '0b0019022001040102010101e0', 'off': '0b001902010000000000000001'}, 
+    'light': {'on': '0b00080280000000000000004b', 'off': '0b000802010000000000000001'}
+}
+
+async def main(address, sensor, switch):
+    chars = sensor_cmds.get(sensor)
+    async with BleakClient(address) as client:
+        print('Connected to:', address)
+        battery_byte = await client.read_gatt_char(battery_char)
+        battery_level = int.from_bytes(battery_byte, byteorder='big', signed=False)
+        print(f"Battery level: {battery_level}%")
+        cmd = bytes.fromhex(chars.get(switch))
+        for _ in range(10000):
+            await client.write_gatt_char(sensor_char, cmd)
+
+def connect(args):
+    stop = False
+    print('Connecting.',end = '', flush=True)
+    while stop == False:
+        try:
+            print('.', end = '', flush=True)
+            asyncio.run(main(**args))
+            stop = True
+        except KeyboardInterrupt:
+            print('\nExiting.')
+            stop = True
+            exit()
+        except: 
+            pass
+
+if __name__ == "__main__":
+
+    argparse = argparse.ArgumentParser()
+
+    argparse.add_argument(
+        '-sensor',
+        help='Sensor (options: light, sound).',
+        metavar='sensor',
+        required=True
+    )
+    argparse.add_argument(
+        '-switch',
+        help='Switch (options: on, off).',
+        metavar='switch',
+        required=True
+    )
+    args = argparse.parse_args()
+
+    if args.sensor not in ('light', 'sound'):
+        raise ValueError('Sensor must be either light or sound.')
+    if args.switch not in ('on', 'off'):
+        raise ValueError('Switch must be either on or off.')
+
+    args_dict = {'address': address, 'sensor': args.sensor, 'switch': args.switch}
+
+    connect(args_dict)
